1df8785696c062f5b0d8088386af3d7797e23285
jamalchelski <ifista201522035@gmail.com>
2024-06-30 14:50:34 +0700

    create controller,model

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Datas;
+use Illuminate\Http\Request;
+
+class DataController extends Controller
+{
+    public function index()
+    {
+        $datas = Datas::all();
+        return response()->json($datas);
+    }
+
+    public function show($id)
+    {
+        $data = Datas::find($id);
+        return response()->json($data);
+    }
+
+    public function create(Request $request)
+    {
+        $data = new Datas();
+        $data->suhu_in = $request->suhu_in;
+        $data->suhu_out = $request->suhu_out;
+        $data->kelembaban_out = $request->kelembaban_out;
+        $data->save();
+        return response()->json("Data berhasil ditambahkan");
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = Datas::find($id);
+
+        $data->suhu_in = $request->suhu_in;
+        $data->suhu_out = $request->suhu_out;
+        $data->kelembaban_out = $request->kelembaban_out;
+        $data->save();
+        return response()->json("Data berhasil diubah", $data);
+    }
+
+    public function delete($id)
+    {
+        $data = Datas::find($id);
+        $data->delete();
+        return response()->json("Data berhasil dihapus");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@
 |
 */
 
+use App\Http\Controllers\DataController;
+
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/datas', 'DataController@index');
+$router->get('/datas/{id}', 'DataController@show');
+$router->post('/datas', 'DataController@create');
+$router->put('/datas/{id}', 'DataController@update');
+$router->delete('/datas/{id}', 'DataController@delete');
+
+// $router->get('/datas', [DataController::class, 'index']);
+// $router->get('/datas/{id}', [DataController::class, 'show']);
+// $router->post('/datas', [DataController::class, 'create']);
+// $router->put('/datas/{id}', [DataController::class, 'update']);
+// $router->delete('/datas/{id}', [DataController::class, 'delete']);
